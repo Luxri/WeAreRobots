324b50f5adfef745ffbac98ca201f302bdd34b49
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2020-01-21 09:10:27 +0100

    Ball created.

---
+++ src/Ball.java
@@ -0,0 +1,14 @@
+public class Ball {
+    private int x;
+    private int y;
+    private int vx;
+    private int vy;
+    private int width;
+    private int height;
+    private String color;
+}
+
+
+
+
+
