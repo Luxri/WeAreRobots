ad8d385ee76e790a1716b19b2046d4c8ede82bc5
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2020-01-21 09:12:13 +0100

    Generator and getters+setters

--- src/Ball.java
+++ src/Ball.java
@@ -6,6 +6,43 @@ public class Ball {
     private int width;
     private int height;
     private String color;
+
+    public Ball(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
 
 
